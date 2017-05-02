84658c6327b2a8d7ee3f152f7af4b794b284cf6e
Yudiz Solutions Pvt. Ltd <chirag@yudiz.com>
2017-05-02 13:02:26 +0530

    no message

---
+++ Assets/Scripts/Ludo/Model/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Games.Ludo{
+	
+
+
+	public 	enum RegionType { Yellow, Green, Red, Blue };
+
+}
---
+++ Assets/Scripts/Ludo/Model/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Games.Ludo{
+
+public class Tile : MonoBehaviour {
+
+		public int tileNumber;
+
+		RegionType regionType;
+
+
+}
+}

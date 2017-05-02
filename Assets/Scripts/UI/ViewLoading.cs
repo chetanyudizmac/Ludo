3ca5038ad653333179ab1e29d775f977c3becfcb
Yudiz Solutions Pvt. Ltd <chirag@yudiz.com>
2017-05-02 17:48:18 +0530

    Merge branch 'master' of https://github.com/chetanyudizmac/Ludo
    # Conflicts:
    #	Assets/Scenes/Ludo 1.unity
    #	Assets/Scenes/Ludo 1.unity.meta

---
+++ Assets/Scripts/UI/ViewLoading.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewLoading : UIView 
+{
+	public static ViewLoading instance;
+	public override void Awake ()
+	{
+		base.Awake ();
+		instance = this;
+	}
+	public override void Show()
+	{
+		base.Show ();
+		StartCoroutine (StartWelcomeScreen());
+
+	}
+	public override void Hide()
+	{
+		base.Hide();
+	}
+	IEnumerator StartWelcomeScreen()
+	{
+		yield return new WaitForSeconds (3f);
+		ViewController.instance.ChangeView (ViewController.instance.viewWelcome);
+	}
+}
---
+++ Assets/Scripts/UI/ViewWelcome.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ViewWelcome : UIView 
+{
+	public static ViewWelcome instance;
+	public Image[] buttonsImage;
+	public Color[] buttonColors;
+	int currentSelectedbutton;
+	Image currentSelectedButtonImage;
+	public override void Awake ()
+	{
+		base.Awake ();
+		instance = this;
+	}
+	public override void Show()
+	{
+		base.Show ();
+		Debug.Log ("Inside View Welcome");
+		currentSelectedButtonImage = buttonsImage [0];
+
+	}
+	public override void Hide()
+	{
+		base.Hide();
+	}
+	public void SelectColorButtonClick(int buttonNo)
+	{
+		currentSelectedButtonImage.color = Color.white;
+		currentSelectedButtonImage = buttonsImage [buttonNo];
+		buttonsImage [buttonNo].color = buttonColors [buttonNo];
+		currentSelectedbutton = buttonNo;
+		Debug.Log ("Button Clicked");
+	}
+
+}
